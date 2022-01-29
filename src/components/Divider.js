97019ef9c8a47190f9a916ab8e285a18a59b1ef6
HuberAdrian <huberadrian@web.de>
2022-01-29 20:04:40 +0100

    added divider component

---
+++ src/components/Divider.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Divider.css';
+
+function Divider() {
+    return (
+        <div class='divider-div'>
+            <h4>Less used</h4>
+            <hr class="divider"></hr>
+        </div>
+    )
+}
+
+export default Divider
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,25 @@ import React from 'react';
 import Direction from './Direction';
 import "./Home.css"
 import SearchBar from './SearchBar';
+import Divider from './Divider';
 
 function Home() {
     return (
       <div className='Home'>
         <h1>Selfmade Github Copilot*</h1>
         <h4>*for Web development</h4>
+
         <SearchBar placeholder="Search for keywords" data="test" />
-        <Direction caption="HTML, CSS" description="Cheatsheets, Templates, ..." colour="red" />
-        <Direction caption="JavaScript" description="Cheatsheets, Templates, ..." colour="green" />
+        <Direction caption="Plan, Start a Project" description="CRA Doc, Inspiration, ..." />
+        <Direction caption="HTML, CSS" description="Templates, UI Components, Cheatsheets..." />
+        <Direction caption="JavaScript" description="Cheatsheets, Templates, Docs..." />
+        <Direction caption="Media" description="Pictures, Videos, Fonts, Icons, Chart libraries ..." />
+        <Direction caption="Git / GitHub"/>
+        <Direction caption="React" description="Cheatsheets, Templates, Docs..." />
+
+        
+        <Divider />
+
       </div>
     );
   }
