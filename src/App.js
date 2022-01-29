443ddf39acb413c821dfdf828be825c2966467e6
HuberAdrian <huberadrian@web.de>
2022-01-29 22:01:17 +0100

    added links with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Home from './components/Home.js';
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+  <div className="App">
+    <Routes>
+    <Route exact path="/" element={<Home/>} />
+    </Routes>
+
+  </div>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import Direction from './Direction';
-import "./Home.css"
-import SearchBar from './SearchBar';
 import Divider from './Divider';
+import SearchBar from './SearchBar';
+import "./Home.css"
+import {Routes, Route, Link} from "react-router-dom";
 
 function Home() {
     return (
-      <div className='Home'>
-        <h1>Selfmade Github Copilot*</h1>
-        <h4>*for Web development</h4>
+    <div>    
+    <h1>Selfmade Github Copilot*</h1>
+    <h4>*for Web development</h4>
 
-        <SearchBar placeholder="Search for keywords" data="test" />
-        <Direction caption="Plan, Start a Project" description="CRA Doc, Inspiration, ..." />
-        <Direction caption="HTML, CSS" description="Templates, UI Components, Cheatsheets..." />
-        <Direction caption="JavaScript" description="Cheatsheets, Templates, Docs..." />
-        <Direction caption="Media" description="Pictures, Videos, Fonts, Icons, Chart libraries ..." />
-        <Direction caption="Git / GitHub"/>
-        <Direction caption="React" description="Cheatsheets, Templates, Docs..." />
+    <SearchBar placeholder="Search for keywords" data="test" />
 
-        <Divider />
+    <Link to="/Plan-Start">
+      <Direction caption="Plan, Start a Project" description="CRA Doc, Inspiration, ..." />
+    </Link>
+    <Link to="/HTML-CSS">
+      <Direction caption="HTML, CSS" description="Templates, UI Components, Cheatsheets..." />
+    </Link>
+    <Link to="/JavaScript">
+      <Direction caption="JavaScript" description="Cheatsheets, Templates, Docs..." />
+    </Link>
+    <Link to="/Media">
+      <Direction caption="Media" description="Pictures, Videos, Fonts, Icons, Chart libraries ..." />
+    </Link> 
+    <Link to="/Git-Github">
+      <Direction caption="Git / GitHub"/>
+    </Link>
+    <Link to="/React">
+      <Direction caption="React" description="Cheatsheets, Templates, Docs..." />
+    </Link>     
+        
+    <Divider />
 
-        <Direction caption="SEO & Webperformance" />
-        <Direction caption="Interview Preparation" description="HTML, CSS, JS, React, Data Structures, Algorithms,..." />
-        <Direction caption="Playing Coding Games" />        
+    <Link to="/SEO">
+      <Direction caption="SEO & Webperformance" />
+    </Link>
+    <Link to="/Interview">
+      <Direction caption="Interview Preparation" description="HTML, CSS, JS, React, Data Structures, Algorithms,..." />
+    </Link>
+    <Link to="/SEO">
+      <Direction caption="Playing Coding Games" />
+    </Link>
+        
+                
 
 
       </div>
