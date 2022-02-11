71c53e2bb5a9e41f582715764983007a592c8bc6
HuberAdrian <huberadrian@web.de>
2022-02-11 03:01:56 +0100

    tried to fix react-router

--- src/Git_Github/How_git_works.js
+++ src/Git_Github/How_git_works.js
@@ -1,23 +1,29 @@
 import React, {useEffect, useState} from 'react';
 
-
-
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import { NotionRenderer } from 'react-notion';
 
 function How_git_works() {
+
     const [data, setData] = useState();
 
+
     useEffect(() => {
-        fetch("https://notion-api.splitbee.io/v1/page/001e5bd586fb4a71a8303498527171be")
+       fetch("https://notion-api.splitbee.io/v1/page/001e5bd586fb4a71a8303498527171be")
         .then(response => response.json())
         .then(dat => {
             console.log(dat)
             setData(dat);
-        });
+        }); 
+
+       
     }, []);
 
     
   return <div>
-      
+     
+      <NotionRenderer blockMap={data} fullPage={false} darkMode={false} />
   </div>;
 }
 
