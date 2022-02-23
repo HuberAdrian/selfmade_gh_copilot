4c58a7834af073ca206514c193ed3aa4c41113e8
HuberAdrian <huberadrian@web.de>
2022-02-23 11:08:34 +0100

    added Directions to JavaScript Page

--- src/JavaScript/JavaScript_Dir.js
+++ src/JavaScript/JavaScript_Dir.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Direction from '../components/Direction';
+import Navbar from '../components/Navbar';
 
 function JavaScript_Dir() {
-  return <div></div>;
+  return <div>
+    <Navbar />
+    <Link to="/">
+      <Direction caption="Documentation" description="" />
+    </Link>
+    <Link to="/">
+      <Direction caption="Basics (personal notes)" description="Operatons on strings, Arrays, Objects, Classes" />
+    </Link>
+    <Link to="/">
+      <Direction caption="Asynchronous callbacks" description="" />
+    </Link>
+    <Link to="/">
+      <Direction caption="Fetch API and JSON" description="Buttons, Navbars" />
+    </Link>
+    <Link to="/">
+      <Direction caption="Best practices" description="" />
+    </Link>
+  </div>;
 }
 
 export default JavaScript_Dir;
