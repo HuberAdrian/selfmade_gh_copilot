f2a131a0df2c8e13d13b2fc9e255d0ab68f05a37
HuberAdrian <huberadrian@web.de>
2022-02-25 14:25:03 +0100

    modified notion, added Links to advanced JS direction

--- src/JavaScript/JS_Advanced.js
+++ src/JavaScript/JS_Advanced.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Direction from '../components/Direction';
+import Navbar from '../components/Navbar';
 
 function JS_Advanced() {
   return (
-    <div>JS_Advanced</div>
+    <div>
+      <Navbar />
+      <Link to="/JavaScript/Advanced/Nullish-Coalescing">
+        <Direction caption="Nullish coalescing" description="?? operator, allows to default to a certain value" />
+      </Link>
+      <Link to="/JavaScript/Advanced/Try-Catch">
+        <Direction caption="Try...catch" description="" />
+      </Link>
+      <Link to="/JavaScript/Advanced/Asynchronous-Callbacks">
+        <Direction caption="Asynchronous Callbacks" description="" />
+      </Link>
+      <Link to="/JavaScript/Advanced/Fetch-and-JSON">
+        <Direction caption="Fetch APi & JSON" description="" />
+      </Link>
+      <Link to="/JavaScript/Advanced/ES-Modules">
+        <Direction caption="ES Modules" description="Export, Import JS code, dynamic Imports" />
+      </Link>
+      <a href="https://elderly-forsythia-b52.notion.site/HTML-JavaScript-76b981a7c2164d6b81cc1b8ab1951049" rel="noopener noreferrer" target="_blank">
+      <Direction caption="HTML in JS" description="select elements, perform actions" />
+    </a>
+      
+      <Link to="/JavaScript/Advanced/forLoop">
+        <Direction caption="for loop" description=" counting, loop over arrays" />
+      </Link>
+      <a href="https://elderly-forsythia-b52.notion.site/Intro-to-Web-Components-f5439fe15535407f899d2df851192fb5" rel="noopener noreferrer" target="_blank">
+      <Direction caption="Web Components" description="create your own reusable HTML elements" />
+    </a>
+      <Link to="/JavaScript/Advanced/IFFEE">
+        <Direction caption="IFFEE" description="functions wrapped in brackets (), legacy code " />
+      </Link>
+    </div>
   )
 }
 
