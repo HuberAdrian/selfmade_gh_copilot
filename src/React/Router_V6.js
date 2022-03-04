7070e6899a7aba9eacc8867bca674b998a3f53e3
HuberAdrian <huberadrian@web.de>
2022-03-04 15:43:59 +0100

    added Doc link to React Router V6

--- src/React/Router_V6.js
+++ src/React/Router_V6.js
@@ -7,13 +7,15 @@ function Router_V6() {
   return (
     <div>
         <Navbar />
+        <a href="https://reactrouter.com/docs/en/v6/getting-started/overview" rel="noopener noreferrer" target="_blank">
+            <Direction caption="Documentation" description="Jest, etc" />
+        </a> 
         <Link to="/React/Tools-Libraries/React-Router-Dom/V4/Intro">
             <Direction caption="Intro React-Router-Dom (V4)" description="navigate within a react-app" />
         </Link>
         <Link to="/React/Tools-Libraries/React-Router-Dom/V6/Redirect">
             <Direction caption="Redirect" description="to another Link" />
         </Link>
-        
     </div>
   )
 }
