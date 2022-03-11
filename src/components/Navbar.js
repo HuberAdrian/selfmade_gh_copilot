37906b65b1b8a372cc59883e4035ab2efd8f82c1
HuberAdrian <huberadrian@web.de>
2022-03-11 11:35:34 +0100

    removed 2nd searchbar in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,19 +5,13 @@ import "./Navbar.css";
 //displays Path, go back and home on the direction pages
 
 function Navbar() {
-    const [search, setSearch] = useState("");
     const navigate = useNavigate();
     const location = useLocation();
-    
-  function handleSearchChange (event) {
-    window.find(event)
-  }
 
   return <div className='Navbar'>
             <p>{location.pathname}</p>
             <button onClick={() => navigate(-1)}>back</button>
             <button onClick={() => navigate("/")} >Home</button>
-            <input type="text" placeholder='Search on page' onChange={event => handleSearchChange(event.target.value)} />
         </div>;
 }
 
