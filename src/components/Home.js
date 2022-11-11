d54a97fab44587a169028c63011de23a8fae3767
HuberAdrian <huberadrian@web.de>
2022-11-11 11:33:00 +0100

    changed links in home directory

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,22 +20,22 @@ function Home() {
 
 
     {/* Links with Directions */}
-    <Link to="/Plan-Start">
+    <Link to="/Copilot/Plan-Start">
       <Direction home={false} caption="Plan, Start a Project" description="CRA Doc, Inspiration, ..." />
     </Link>
-    <Link to="/HTML-CSS">
+    <Link to="/Copilot/HTML-CSS">
       <Direction home={false} caption="HTML, CSS" description="Templates, UI Components, Cheatsheets..." />
     </Link>
-    <Link to="/JavaScript">
+    <Link to="/Copilot/JavaScript">
       <Direction home={false} caption="JavaScript" description="Cheatsheets, Docs, best Practices..." />
     </Link>
-    <Link to="/React">
+    <Link to="/Copilot/React">
       <Direction home={false} caption="React" description="Cheatsheets, Templates, Docs..." />
     </Link> 
-    <Link to="/Git-Github">
+    <Link to="/Copilot/Git-Github">
       <Direction home={false} caption="Git / GitHub"/>
     </Link>
-    <Link to="/Media">
+    <Link to="/Copilot/Media">
       <Direction home={false} caption="Media" description="Pictures, Videos, Fonts, Icons, Chart libraries ..." />
     </Link> 
     <a href="https://elderly-forsythia-b52.notion.site/Intro-to-testing-28e8b1b457b742cfbb4e9e289e70384e" rel="noopener noreferrer" target="_blank">
@@ -45,10 +45,10 @@ function Home() {
     <Divider home={false} />
 
     {/* Links with Directions for less used directions*/}
-    <Link to="/SEO">
+    <Link to="/Copilot/SEO">
       <Direction home={false} caption="SEO & Webperformance" />
     </Link>
-    <Link to="/Interview">
+    <Link to="/Copilot/Interview">
       <Direction home={false} caption="Interview Preparation" description="HTML, CSS, JS, React, Data Structures, Algorithms,..." />
     </Link>
     <a href="https://serverless.css-tricks.com" rel="noopener noreferrer" target="_blank">
